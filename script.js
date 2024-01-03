19281edef75c7e8366ea032019b427cc22caf915
aronsn <aron@snekkestad.com>
2024-01-03 21:35:02 +0100

    feat: add onload functions to all html docs, add function for retrieving answer value and append to element, and add finishing styling for feedback card

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-document.querySelectorAll('.radio-button').forEach(item => {
-    const input = item.querySelector('input')
-    const label = item.querySelector('label')
+const addClickEventsToButtons = () => {
+    // fetch all radio buttons
+    document.querySelectorAll('.radio-button').forEach(item => {
+        const input = item.querySelector('input')
+        const label = item.querySelector('label')
 
-    label.addEventListener('click', event => {
-        //handle click
-        input.checked = true;
+        // add a click event listener
+        label.addEventListener('click', event => {
+            // handle click
+            input.checked = true;
 
+        })
     })
-})
+}
+
+const retrieveAnswerValue = () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const myParam = queryParams.get('answer');
+    return myParam;
+}
+
+const appendToElement = () => {
+    const target = document.getElementById('target-element');
+    const value = retrieveAnswerValue();
+    target.innerHTML = value;
+}
