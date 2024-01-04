3965f13441111bbd87db155264aa0c8582e27794
aronsn <aron@snekkestad.com>
2024-01-04 13:39:59 +0100

    feat: FM feedback 1. add submit validation

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const addClickEventsToButtons = () => {
     document.querySelectorAll('.radio-button').forEach(item => {
         const input = item.querySelector('input')
         const label = item.querySelector('label')
-
         // add a click event listener
         label.addEventListener('click', event => {
             // handle click
             input.checked = true;
-
         })
     })
 }
@@ -23,4 +21,18 @@ const appendToElement = () => {
     const target = document.getElementById('target-element');
     const value = retrieveAnswerValue();
     target.innerHTML = value;
+}
+
+const validateSubmission = () => {
+    // fetch form
+    const form = document.getElementsByClassName('rating-form')[0];
+    // add a submit event listener
+    form.addEventListener('submit', (event) => {
+        // handle submission
+        const answer = form.answer.value;
+        if (answer === "") {
+            event.preventDefault();
+            alert("No answer is no good feedback!")
+        }
+    })
 }
